e46927fc9668ebbdafdc3ed828734d62534d61ce
kristenmills <kristen@kristen-mills.com>
2014-02-02 18:13:26 -0500

    Added socket io stuff

--- logger.js
+++ logger.js
@@ -1,6 +1,8 @@
 var mycroft = require('./mycroft.js');
 var client = mycroft.Mycroft('app.json', 'localhost', 1847);
 var logs = [];
+var port = 7777;
+var io = require('socket.io').listen(port);
 
 client.on('APP_MANIFEST_OK', function(data){
   client.appManifestOk();
@@ -18,12 +20,14 @@ client.on('APP_DEPENDENCY', function(data){
   client.up();
 });
 
-client.on('MSG_QUERY', function(data){
-  if(data.action == 'log'){
-    logs.push(data.data);
-  }
+client.on('MSG_BROADCAST', function(data){
+  logs.push(data.content);
+  io.sockets.emit('log', data.content);
 });
 
+io.sockets.on('connection' function(socket){
+  socket.emit('initial logs', logs);
+});
 
 client.connect();
 client.sendManifest();
